4662cc1f7d813bd71132e9e862b7cdbd24c39cc7
Nikita <nikita.vichurin@gmail.com>
2025-04-02 01:32:20 +0400

    Готовая первая половина

---
+++ lab_1/constants.py
@@ -0,0 +1,10 @@
+ALPHABET = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+PATH_TO_TEXT_FILE = "original_text.txt"
+PATH_TO_KEY_FILE = "key.txt"
+PATH_TO_WRITE_TEXT_FILE = "encrypted_text.txt"
+MATRIX = [
+	[None, None, None, None, None, None, None, None],
+	[None, None, None, None, None, None, None, None],
+	[None, None, None, None, None, None, None, None],
+	[None, None, None, None, None, None, None, None]
+]
