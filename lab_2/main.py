24f99f6771276e66f9b19ab264881c546c0bde5e
Nikita <nikita.vichurin@gmail.com>
2025-05-03 19:51:08 +0400

    Тест на одинаковые подряд идущие биты.

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,4 +1,6 @@
 import nist_test
+import const
+
 
 def read_file(filename: str):
 	with open(filename, "r") as file:
@@ -8,7 +10,7 @@ def read_file(filename: str):
 
 def write_results(freq_cpp, freq_java,
 				  runs_cpp, runs_java,
-				  long_cpp, long_java,
+				  # long_cpp, long_java,
 				  results: str):
 	with open(results, 'w') as file:
 		file.write("Frequency bitwise test:\n")
@@ -19,12 +21,17 @@ def write_results(freq_cpp, freq_java,
 		file.write(f"cpp: {runs_cpp}\n")
 		file.write(f"java: {runs_java}\n\n")
 
-		file.write("Test for the longest sequence of units in a block:\n")
-		file.write(f"cpp: {long_cpp}\n")
-		file.write(f"java: {long_java}\n")
+		# file.write("Test for the longest sequence of units in a block:\n")
+		# file.write(f"cpp: {long_cpp}\n")
+		# file.write(f"java: {long_java}\n")
 
 
 def main():
+	freq_cpp = nist_test.frequency_test(read_file(const.bin_seq_cpp))
+	freq_java = nist_test.frequency_test(read_file(const.bin_seq_java))
+	runs_cpp = nist_test.runs_test(read_file(const.bin_seq_cpp))
+	runs_java = nist_test.runs_test(read_file(const.bin_seq_java))
+	write_results(freq_cpp,freq_java,runs_cpp,runs_java,const.res)
 
 
 
--- lab_2/nist_test.py
+++ lab_2/nist_test.py
@@ -1,14 +1,34 @@
 import math
 
+
 def frequency_test(bits):
 	sum_bit = 0
 	for bit in bits:
 		if bit == "0":
 			sum_bit += -1
 		else:
-			sum_bit +=1
+			sum_bit += 1
 	normal_sum = sum_bit / math.sqrt(len(bits))
 	p_value = math.erfc(normal_sum / math.sqrt(2))
 	return p_value
 
 
+def runs_test(bits):
+	share_units = 0
+	for bit in bits:
+		if bit == "1":
+			share_units += 1
+	share_units = share_units / len(bits)
+
+	if abs(share_units - 0.5 >= math.sqrt(len(bits))):
+		return 0.0
+
+	series = 0
+	for i in range(len(bits) - 1):
+		if bits[i] != bits[i + 1]:
+			series += 1
+
+	numerator = abs(series - 2 * len(bits) * share_units * (1 - share_units))
+	denominator = 2 * math.sqrt(2 * len(bits)) * share_units * (1 - share_units)
+	p_value = math.erfc(numerator / denominator)
+	return p_value
