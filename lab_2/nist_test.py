5592b7e66f788fe28472cc1462d13ddd6b27b6db
VichurinNik <162898652+VichurinNik@users.noreply.github.com>
2025-05-05 17:05:51 +0400

    Update nist_test.py

--- lab_2/nist_test.py
+++ lab_2/nist_test.py
@@ -20,7 +20,7 @@ def frequency_test(bits):
 			sum_bit += -1
 		else:
 			sum_bit += 1
-	normal_sum = sum_bit / math.sqrt(len(bits))
+	normal_sum = abs(sum_bit) / math.sqrt(len(bits))
 	p_value = math.erfc(normal_sum / math.sqrt(2))
 	return p_value
 
