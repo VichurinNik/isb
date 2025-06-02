8de15372f7a84cacb771d30852b3ad3a30dadbc0
VichurinNik <162898652+VichurinNik@users.noreply.github.com>
2025-06-03 00:13:58 +0400

    Update const.py

--- lab_3/main/const.py
+++ lab_3/main/const.py
@@ -1,7 +1,7 @@
-PATH_TO_PLAINTEXT = "D:\OIb_lab3\isb\lab_3\\text\plaintext.txt"
-PATH_TO_CIPHERTEXT = "D:\OIb_lab3\isb\lab_3\\text\ciphertext.txt"
-PATH_TO_ENCRYPTED_TEXT = "D:\OIb_lab3\isb\lab_3\\text\encrypted_text.txt"
+PATH_TO_PLAINTEXT = "text\plaintext.txt"
+PATH_TO_CIPHERTEXT = "text\ciphertext.txt"
+PATH_TO_ENCRYPTED_TEXT = "text\encrypted_text.txt"
 
-PATH_TO_SYM_KEY = "D:\OIb_lab3\isb\lab_3\key\summetric_key.txt"
-PATH_TO_PRIVATE_KEY = "D:\OIb_lab3\isb\lab_3\key\private_key.txt"
-PATH_TO_PUBLIC_KEY = "D:\OIb_lab3\isb\lab_3\key\public_key.txt"
+PATH_TO_SYM_KEY = "key\summetric_key.txt"
+PATH_TO_PRIVATE_KEY = "key\private_key.txt"
+PATH_TO_PUBLIC_KEY = "key\public_key.txt"
